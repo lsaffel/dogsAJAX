a8af402e8dc86a5b5acfea8803289f44d66bd326
Lisa <llvvss@icloud.com>
2021-12-24 10:18:37 -0700

    added a try catch

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 async function makeCalls() {
-  const response = await fetch("https://dog.ceo/api/breeds/list/all");
-  // the next line is not executed until that fetch completes
-  // Fetches a list of dog breeds from a public API
+  try {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    // the next line is not executed until that fetch completes
+    // Fetches a list of dog breeds from a public API
 
-  const dogBreedsList = await response.json();
-  // the next line below is not executed until the line above completes
+    const dogBreedsList = await response.json();
+    // the next line below is not executed until the line above completes
 
-  console.log(dogBreedsList); // log the list of breeds to the console
+    console.log(dogBreedsList); // log the list of breeds to the console
+  } catch (error) {
+    console.log("The data was not retrievable");
+  }
 }
 
 makeCalls();
